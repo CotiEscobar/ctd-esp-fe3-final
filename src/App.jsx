862a01135d2064e167d560c2133e0f84e4a21f5b
CotiEscobar <constanzaescobar15@gmail.com>
2023-04-04 15:01:47 -0300

    creacion de rutas

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
-
 import Footer from "./Components/Footer";
 import Navbar from "./Components/Navbar";
+import { Routes, Route } from "react-router-dom";
+import Home from "./Routes/Home";
+import Detail from "./Routes/Detail";
+import Contact from "./Routes/Contact"
+import Favs from "./Routes/Favs"
 
 
 function App() {
   return (
-      <div className="App">
-          <Navbar/>
-          <Footer/>
-      </div>
+    <div className="App">
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/dentist/:id" element={<Detail />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/favs" element={<Favs />} />
+      </Routes>
+
+      <Navbar/>
+      <Footer/>
+    </div>
   );
 }
 
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,13 +1,21 @@
 import React from 'react'
+import { Link, useNavigate } from 'react-router-dom'
 
 //Este componente debera ser estilado como "dark" o "light" dependiendo del theme del Context
 
 const Navbar = () => {
+  const navigate = useNavigate()
 
   return (
     <nav>
       {/* Aqui deberan agregar los liks correspondientes a las rutas definidas */}
       {/* Deberan implementar ademas la logica para cambiar de Theme con el button */}
+      <Link to="/home"><h3>Home</h3></Link>
+      <Link to="/dentist/:id"><h3>Dentist</h3></Link>
+      <Link to="/contact"><h3>Contact</h3></Link>
+      <Link to="/favs"><h3>Favs</h3></Link>
+      <button onClick={() => navigate(-3)}>↩</button>
+
       <button>Change theme</button>
     </nav>
   )
